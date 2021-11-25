3a840641be1263c092476aadbfe37851279f6582
Vittorio Rinaldi <vittorio.rinaldi@studenti.unicam.it>
2021-11-25 17:48:45 +0100

    commento

--- src/test/java/pros/unicam/spm2021/practice/Selenium/SeleniumTest.java
+++ src/test/java/pros/unicam/spm2021/practice/Selenium/SeleniumTest.java
@@ -1,126 +1,126 @@
-package pros.unicam.spm2021.practice.Selenium;
+// package pros.unicam.spm2021.practice.Selenium;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Assert;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+// import org.junit.Assert;
+// import org.junit.jupiter.api.AfterAll;
+// import org.junit.jupiter.api.AfterEach;
+// import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Disabled;
+// import org.junit.jupiter.api.Test;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
 
-class SeleniumTest {
+// class SeleniumTest {
 
-	static String browser;
-	static String projectPath;
+// 	static String browser;
+// 	static String projectPath;
 	
-	static WebDriver driver;
+// 	static WebDriver driver;
 	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-		 projectPath = System.getProperty("user.dir");
-	}
+// 	/**
+// 	 * @throws java.lang.Exception
+// 	 */
+// 	@BeforeAll
+// 	static void setUpBeforeClass() throws Exception {
+// 		 projectPath = System.getProperty("user.dir");
+// 	}
 	
 	
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {		
-	}
+// 	/**
+// 	 * @throws java.lang.Exception
+// 	 */
+// 	@AfterAll
+// 	static void tearDownAfterClass() throws Exception {		
+// 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception {
-		if(System.getProperty("os.name").equals("Mac OS X")) {
-			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/mac/chromedriver");
-		}
-		if(System.getProperty("os.name").contains("Windows")) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\studente\\Documents\\BrowserDriver\\chromedriver.exe");
-		}
-		driver = new ChromeDriver();
-	}
+// 	/**
+// 	 * @throws java.lang.Exception
+// 	 */
+// 	@BeforeEach
+// 	void setUp() throws Exception {
+// 		if(System.getProperty("os.name").equals("Mac OS X")) {
+// 			System.setProperty("webdriver.chrome.driver", projectPath+"/drivers/mac/chromedriver");
+// 		}
+// 		if(System.getProperty("os.name").contains("Windows")) {
+// 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\studente\\Documents\\BrowserDriver\\chromedriver.exe");
+// 		}
+// 		driver = new ChromeDriver();
+// 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception {
-		driver.close();
-		driver.quit();
-	}
+// 	/**
+// 	 * @throws java.lang.Exception
+// 	 */
+// 	@AfterEach
+// 	void tearDown() throws Exception {
+// 		driver.close();
+// 		driver.quit();
+// 	}
 
-	@Test
-	//@Disabled
-	void checkProsSite() throws InterruptedException {
+// 	@Test
+// 	//@Disabled
+// 	void checkProsSite() throws InterruptedException {
 		
-		driver.get("http://pros.unicam.it/");
+// 		driver.get("http://pros.unicam.it/");
 
-		Thread.sleep(3000);
+// 		Thread.sleep(3000);
 		
-		String at = driver.getTitle();
-		String et = "PROS[ - PROcesses & Services lab | Computer Science division @Unicam]";
+// 		String at = driver.getTitle();
+// 		String et = "PROS[ - PROcesses & Services lab | Computer Science division @Unicam]";
 		
-		//System.out.println(at);		
-		Thread.sleep(4000);
+// 		//System.out.println(at);		
+// 		Thread.sleep(4000);
 
-		Assert.assertEquals(et,at);
+// 		Assert.assertEquals(et,at);
 		
 		
-	}
+// 	}
 	
-	@Test
-	@Disabled
-	void checkProsSiteSearch() throws InterruptedException {
+// 	@Test
+// 	@Disabled
+// 	void checkProsSiteSearch() throws InterruptedException {
 		
-		// navigate your driver to mail.google.com
-		driver.get("http://pros.unicam.it/");
+// 		// navigate your driver to mail.google.com
+// 		driver.get("http://pros.unicam.it/");
 
-		Thread.sleep(4000);
+// 		Thread.sleep(4000);
 		
-		//Find Element by className
-		driver.findElement(By.className("toggle-search")).click();
+// 		//Find Element by className
+// 		driver.findElement(By.className("toggle-search")).click();
 		
-		Thread.sleep(4000);
+// 		Thread.sleep(4000);
 		
-		driver.findElement(By.className("search")).click();
+// 		driver.findElement(By.className("search")).click();
 		
-		Thread.sleep(4000);
+// 		Thread.sleep(4000);
 		
-		driver.findElement(By.className("search")).sendKeys("bprove");
+// 		driver.findElement(By.className("search")).sendKeys("bprove");
 		
-		Thread.sleep(4000);
+// 		Thread.sleep(4000);
 		
-		//TO COMPLETE
+// 		//TO COMPLETE
 			
-	}
+// 	}
 	
-	@Test
-	@Disabled
-	void checkThisWebAppTitle() throws InterruptedException {
+// 	@Test
+// 	@Disabled
+// 	void checkThisWebAppTitle() throws InterruptedException {
 		
-		driver.get("http://localhost:80/spm2021/");
+// 		driver.get("http://localhost:80/spm2021/");
 
-		Thread.sleep(3000);
+// 		Thread.sleep(3000);
 		
-		String at = driver.getTitle();
-		String et = "SPM 2021";
+// 		String at = driver.getTitle();
+// 		String et = "SPM 2021";
 		
-		//System.out.println(at);		
-		Thread.sleep(4000);
+// 		//System.out.println(at);		
+// 		Thread.sleep(4000);
 
-		Assert.assertEquals(et,at);
+// 		Assert.assertEquals(et,at);
 		
 		
-	}
+// 	}
 
-}
+// }
